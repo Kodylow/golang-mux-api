b031c520142aafdf103543256e75795eade46620
Kody Low <kodylow7@gmail.com>
2021-10-28 12:03:03 -0700

    finished restful api with go

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -46,17 +48,43 @@ func getBook(w http.ResponseWriter, r *http.Request) {
 
 //Create a Book
 func createBook(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	var b Book
+	_ = json.NewDecoder(r.Body).Decode(&b)
+	b.ID = strconv.Itoa(rand.Intn(10000000))
+	books = append(books, b)
+	json.NewEncoder(w).Encode(b)
 }
 
 //Update a Book
 func updateBook(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for ind, i := range books {
+		if i.ID == params["ID"] {
+			books = append(books[:ind], books[ind+1:]...)
+			var b Book
+			_ = json.NewDecoder(r.Body).Decode(&b)
+			b.ID = strconv.Itoa(rand.Intn(10000000))
+			books = append(books, b)
+			json.NewEncoder(w).Encode(b)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(books)
 }
 
 //Delete a Book
 func deleteBook(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for ind, i := range books {
+		if i.ID == params["ID"] {
+			books = append(books[:ind], books[ind+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(books)
 }
 
 func main() {
@@ -66,7 +94,7 @@ func main() {
 	//Mock Data
 	books = append(books, Book{ID: "1", Isbn: "4487", Title: "Book One", Author: &Author{Firstname: "Kody", Lastname: "Low"}},
 		Book{ID: "2", Isbn: "4497", Title: "Book Two", Author: &Author{Firstname: "Kody", Lastname: "Low"}})
-	fmt.Println(books)
+	fmt.Println("Starting server...")
 	//Route Handlers / Endpoints
 	r.HandleFunc("/api/books", getBooks).Methods("GET")
 	r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
